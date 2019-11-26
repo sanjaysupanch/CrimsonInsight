a90cfc60dc40bcb063750e44a20c349157f0fa13
sanjaysupanch <sanjay.k17@iiits.in>
2019-11-26 21:35:15 +0530

    adding gitignore file

--- crimson/crimson/settings.py
+++ crimson/crimson/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fa@vcnpkvm5rmhpq*^%_y39m286ls%(zk!725@01c$lk8ja$g_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ec2-18-191-22-196.us-east-2.compute.amazonaws.com  ', '127.0.0.1' ]
 
 
 # Application definition
